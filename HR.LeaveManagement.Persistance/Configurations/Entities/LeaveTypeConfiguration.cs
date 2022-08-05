241de140782c373b91606a04f9b80f605ef09bf4
K1 <searchfordeveloping@gmail.com>
2022-08-04 22:37:17 -0700

    added ef configurations for entities and seeding

---
+++ HR.LeaveManagement.Persistance/Configurations/Entities/LeaveTypeConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using HR.LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HR.LeaveManagement.Persistence.Configurations.Entities
+{
+    public class LeaveTypeConfiguration : IEntityTypeConfiguration<LeaveType>
+    {
+        public void Configure(EntityTypeBuilder<LeaveType> builder)
+        {
+            builder.HasData(
+                new LeaveType
+                {
+                    Id = 1,
+                    Name = "Vacation",
+                    DefaultDays = 10,
+                    CreatedBy = "Amin",
+                    DateCreated = DateTime.Now,
+                    LastModifiedBy = "Amin",
+                    LastModifiedDate = DateTime.Now
+                },
+                new LeaveType
+                {
+                    Id = 2,
+                    Name = "Sick",
+                    DefaultDays = 12,
+                    CreatedBy = "Amin",
+                    DateCreated = DateTime.Now,
+                    LastModifiedBy = "Amin",
+                    LastModifiedDate = DateTime.Now
+                }
+            );
+        }
+    }
+}
---
+++ HR.LeaveManagement.Persistance/Migrations/20220805051959_SeedingLeaveTypes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HR.LeaveManagement.Persistence.Migrations
+{
+    public partial class SeedingLeaveTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "LeaveTypes",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DefaultDays", "LastModifiedBy", "LastModifiedDate", "Name" },
+                values: new object[] { 1, "Amin", new DateTime(2022, 8, 4, 22, 19, 59, 555, DateTimeKind.Local).AddTicks(6230), 10, "Amin", new DateTime(2022, 8, 4, 22, 19, 59, 555, DateTimeKind.Local).AddTicks(6268), "Vacation" });
+
+            migrationBuilder.InsertData(
+                table: "LeaveTypes",
+                columns: new[] { "Id", "CreatedBy", "DateCreated", "DefaultDays", "LastModifiedBy", "LastModifiedDate", "Name" },
+                values: new object[] { 2, "Amin", new DateTime(2022, 8, 4, 22, 19, 59, 555, DateTimeKind.Local).AddTicks(6271), 12, "Amin", new DateTime(2022, 8, 4, 22, 19, 59, 555, DateTimeKind.Local).AddTicks(6272), "Sick" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "LeaveTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
