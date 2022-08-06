d3d57d643b90baa9a0ea08049ca575e6ae1c572d
K1 <searchfordeveloping@gmail.com>
2022-08-06 16:32:16 -0700

    Added mvc Ui project

---
+++ HR.LeaveManagement.MVC/Services/Base/BaseHttpService.cs
@@ -0,0 +1,40 @@
+﻿using HR.LeaveManagement.MVC.Contracts;
+using System.Net.Http.Headers;
+
+namespace HR.LeaveManagement.MVC.Services.Base
+{
+    public class BaseHttpService
+    {
+        public BaseHttpService(IClient client, ILocalStorageService localStorageService)
+        {
+            Client = client;
+            LocalStorageService = localStorageService;
+        }
+
+        public IClient Client { get; }
+        public ILocalStorageService LocalStorageService { get; }
+
+        protected Response<Guid> ConvertApiExceptions<Guid>(ApiException apiException)
+        {
+            if (apiException.StatusCode == 400)
+            {
+                return new Response<Guid>() { Message = "Validation errors have occured.", ValidationErrors = apiException.Response, Success = false };
+            }
+            else if (apiException.StatusCode == 404)
+            {
+                return new Response<Guid>() { Message = "The request item could not be found.", Success = false };
+            }
+            else
+            {
+                return new Response<Guid>() { Message = "Something went wrong, please try again.", Success = false };
+            }
+        }
+
+        protected void AddBearerToken()
+        {
+            if (LocalStorageService.Exists("token"))
+                Client.HttpClient.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", LocalStorageService.GetStorageValue<string>("token"));
+        }
+    }
+}
---
+++ HR.LeaveManagement.MVC/Services/LocalStorageService.cs
@@ -0,0 +1,46 @@
+﻿using Hanssens.Net;
+using HR.LeaveManagement.MVC.Contracts;
+
+namespace HR.LeaveManagement.MVC.Services
+{
+    public class LocalStorageService : ILocalStorageService
+    {
+        private LocalStorage _localStorage;
+        public LocalStorageService()
+        {
+            var config = new LocalStorageConfiguration() 
+            { 
+                AutoLoad= true,
+                AutoSave= true,
+                Filename= "HR.LEAVMGMT"
+            };
+            _localStorage = new LocalStorage(config);
+        }
+        public void ClearStorage(List<string> keys)
+        {
+            foreach (var key in keys)
+            {
+                _localStorage.Remove(key);
+            }
+        }
+
+        public bool Exists(string key)
+        {
+            return _localStorage.Exists(key);
+        }
+
+        public T GetStorageValue<T>(string key)
+        {
+            var value = _localStorage.Get(key);
+            if (value == null)
+                return default;
+            return (T)value;
+        }
+
+        public void SetStorageValue<T>(string key, T value)
+        {
+            _localStorage.Store(key, value);
+            _localStorage.Persist();
+        }
+    }
+}
